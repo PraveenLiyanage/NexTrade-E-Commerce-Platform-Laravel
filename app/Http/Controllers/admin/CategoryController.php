c61fe838ce22624d4d2584ea754efcf103b04fbb
Praveen Liyanage <praveenliyanage1@gmail.com>
2024-02-10 16:26:37 +0530

    Update Controllers

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -15,8 +16,20 @@ class CategoryController extends Controller
         return view('admin.category.create');
     }
 
-    public function store(){
-        
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories',
+        ]);
+
+        if ($validator->passes()){
+
+        } else {
+            return response() -> json([
+                'status' => true,
+                'errors' => $validator->errors()
+            ]);
+        }
     }
 
     public function edit(){
